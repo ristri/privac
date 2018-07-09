66b0f260c515f1d7169519c9976ec97351a8391d
Rishabh <ristripathi97@gmail.com>
2018-07-10 00:53:26 +0530

    added sidepanel

---
+++ src/SidePanel.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "bulma/css/bulma.css";
+import QRCode from 'qrcode.react';
+
+export class SidePanel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { showQR: false }
+        this.handleQR = this.handleQR.bind(this);
+    }
+    handleQR() {
+        this.setState({ showQR: !(this.state.showQR) })
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleQR}>Generate QR </button>
+                <div className={this.state.showQR ? "modal is-active" : "modal" }>
+                    <div className="modal-background"></div>
+                    <div className="modal-content">
+                    <QRCode value={window.location.href} size={256} />
+                    </div>
+                    <button onClick={this.handleQR} className="modal-close is-large" aria-label="close"></button>
+                </div>
+            </div>
+        );
+    }
+} 
